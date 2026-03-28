53ff0c7571a9a73ead6632322c10bed937770535
Ryma Djoudad <zenyod85@gmail.com>
2026-03-28 17:45:47 -0400

    Working on constructor

--- Lab10_rdjoudad.py
+++ Lab10_rdjoudad.py
@@ -6,4 +6,13 @@ which will then analyze the frequency of each word and print the information out
 Imported text files containing public domain stories
 03/28/2026
 """
+import pathlib, string
 
+class WordAnalyzer:
+
+    def __init__(self, filepath):
+        # triple docstring goes here
+        self.__filepath = filepath.Path(filepath)
+        self.__word_occurences = {}
+
+    
