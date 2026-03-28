3943f22c09acb33c792d65f5c433fe8aa4f12f57
Ryma Djoudad <zenyod85@gmail.com>
2026-03-28 13:16:07 -0400

    init commit

---
+++ Lab10_rdjoudad.py
@@ -0,0 +1,9 @@
+"""
+Word Count Project
+Ryma Djoudad
+OOP-based program that allows the user to select a file, 
+which will then analyze the frequency of each word and print the information out.
+Imported text files containing public domain stories
+03/28/2026
+"""
+
